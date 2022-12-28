3ed6d5ad8961278f10017eb6a7e8ac392c10da28
LE JEUNE alban <albanlejeune94@gmail.com>
2022-12-28 17:03:25 +0100

    Ajout du dépot du projet

---
+++ app/src/main/java/fr/lejeune/banane/projettraduction/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.lejeune.banane.projettraduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
