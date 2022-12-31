2d9e5ed935fa910aa9a49445858fea8cf86e3c1a
Beshido <albanlejeune94@gmail.com>
2022-12-31 14:26:52 +0100

    Ouais et tout

--- app/src/main/java/fr/lejeune/banane/projettraduction/MainActivity.kt
+++ app/src/main/java/fr/lejeune/banane/projettraduction/MainActivity.kt
@@ -2,10 +2,21 @@ package fr.lejeune.banane.projettraduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import fr.lejeune.banane.projettraduction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root)
+
+        val database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "app_database"
+        ).build()
+        val userDao = database.traductionDao()
     }
 }
